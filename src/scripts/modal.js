a354764ffe07fb22b7ba303923598404fcc393bf
Sergey Akimov <airwalk45@yandex.ru>
2024-03-20 19:27:37 +0300

    20032024 19:27

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,18 +1,17 @@
 // Функция открытия модального окна
 function openModal(DomModal) {
-  DomModal.classList.add('popup_is-opened');
-  DomModal.classList.add('popup_is-animated');
-  document.addEventListener('keydown', function (evt) {
-    if (evt.key === 'Escape') {
+  DomModal.classList.add("popup_is-opened");
+  DomModal.classList.add("popup_is-animated");
+  document.addEventListener("keydown", function (evt) {
+    if (evt.key === "Escape") {
       closeModal(DomModal);
     }
-}); 
-};
-
+  });
+}
 
 // Функция закрытия модального окна
 function closeModal(Modal) {
-  Modal.classList.remove('popup_is-opened');
-};
+  Modal.classList.remove("popup_is-opened");
+}
 
-export {openModal, closeModal};
+export { openModal, closeModal };
